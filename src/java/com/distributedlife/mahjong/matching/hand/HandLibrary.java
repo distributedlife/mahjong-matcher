a19fb10f65bad36bf6c78e07b2d1a67817221921
Ryan Boucher <ryan.boucher@distributedlife.com>
2014-03-25 16:22:27 +0800

    load library using the new file format
    Former-commit-id: bd1542295fd74031200f64b6a1eb6594f050cfc8

--- src/java/com/distributedlife/mahjong/matching/hand/HandLibrary.java
+++ src/java/com/distributedlife/mahjong/matching/hand/HandLibrary.java
@@ -14,14 +14,31 @@ public class HandLibrary {
         JSONArray handsJson = root.getJSONArray("hands");
         for (int i = 0; i < handsJson.length(); i++) {
             JSONObject handJson = handsJson.getJSONObject(i);
+            String name = handJson.getString("name");
 
-            List<String> requiredTiles = new ArrayList<String>();
-            JSONArray requiredTilesAsJson = handJson.getJSONArray("requiredTiles");
-            for (int j = 0; j < requiredTilesAsJson.length(); j++) {
-                requiredTiles.add(requiredTilesAsJson.getString(j));
+            JSONArray combinationsJson = handJson.getJSONArray("combinations");
+            for(int j = 0; j < combinationsJson.length(); j++) {
+                JSONObject combinationJson = combinationsJson.getJSONObject(j);
+
+                long part1 = 0;
+                long part2 = 0;
+                long part3 = 0;
+                long part4 = 0;
+                if (combinationJson.has("1")) {
+                    part1 = combinationJson.getLong("1");
+                }
+                if (combinationJson.has("2")) {
+                    part2 = combinationJson.getLong("2");
+                }
+                if (combinationJson.has("3")) {
+                    part3 = combinationJson.getLong("3");
+                }
+                if (combinationJson.has("4")) {
+                    part4 = combinationJson.getLong("4");
+                }
+
+                hands.add(new Hand(name, part1, part2, part3, part4));
             }
-
-            hands.add(new Hand(handJson.getString("name"), requiredTiles));
         }
 
         return hands;
--- test/java/com/distributedlife/mahjong/matching/hand/HandLibraryTest.java
+++ test/java/com/distributedlife/mahjong/matching/hand/HandLibraryTest.java
@@ -3,58 +3,59 @@ package com.distributedlife.mahjong.matching.hand;
 import com.distributedlife.mahjong.reference.hand.Hand;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.List;
 
+import static com.distributedlife.mahjong.reference.data.TileSet.Tile;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class HandLibraryTest {
     @Test
-    @Ignore
     public void shouldLoadFromJson() throws IOException {
         JSONObject jsonObject = new JSONObject(IOUtils.toString(HandLibraryTest.class.getResourceAsStream("/two-hands.json")));
 
         List<Hand> hands = HandLibrary.loadFromJson(jsonObject);
 
-        assertThat(hands.get(0).getName(), is("Run, Pung and a Pair"));
-//        assertThat(hands.get(0).getRequiredTiles(), is(Arrays.asList(
-//                "1 Bamboo",
-//                "1 Bamboo",
-//                "1 Bamboo",
-//                "1 Bamboo",
-//                "2 Bamboo",
-//                "2 Bamboo",
-//                "2 Bamboo",
-//                "3 Bamboo",
-//                "4 Bamboo",
-//                "5 Bamboo",
-//                "6 Bamboo",
-//                "7 Bamboo",
-//                "8 Bamboo",
-//                "9 Bamboo"
-//        )));
+        assertThat(hands.get(0).getName(), is("Yin Yang"));
+        assertThat(hands.get(0).getPart1(), is(
+            Tile.B1.v + Tile.B2.v + Tile.B3.v + Tile.B4.v + Tile.B5.v +
+            Tile.S5.v + Tile.S6.v + Tile.S7.v + Tile.S8.v + Tile.S9.v
+        ));
+        assertThat(hands.get(0).getPart2(), is(
+            Tile.B1.v + Tile.B5.v +
+            Tile.S5.v + Tile.S9.v
+        ));
+        assertThat(hands.get(0).getPart3(), is(0L));
+        assertThat(hands.get(0).getPart4(), is(0L));
 
-        assertThat(hands.get(1).getName(), is("Run, Pung and a Pair"));
-//        assertThat(hands.get(1).getRequiredTiles(), is(Arrays.asList(
-//                "1 Bamboo",
-//                "1 Bamboo",
-//                "1 Bamboo",
-//                "1 Bamboo",
-//                "2 Bamboo",
-//                "3 Bamboo",
-//                "3 Bamboo",
-//                "3 Bamboo",
-//                "4 Bamboo",
-//                "5 Bamboo",
-//                "6 Bamboo",
-//                "7 Bamboo",
-//                "8 Bamboo",
-//                "9 Bamboo"
-//        )));
+
+        assertThat(hands.get(1).getName(), is("Yin Yang"));
+        assertThat(hands.get(1).getPart1(), is(
+            Tile.B5.v + Tile.B6.v + Tile.B7.v + Tile.B8.v + Tile.B9.v +
+            Tile.S1.v + Tile.S2.v + Tile.S3.v + Tile.S4.v + Tile.S5.v
+        ));
+        assertThat(hands.get(1).getPart2(), is(
+            Tile.B5.v + Tile.B9.v +
+            Tile.S1.v + Tile.S5.v
+        ));
+        assertThat(hands.get(1).getPart3(), is(0L));
+        assertThat(hands.get(1).getPart4(), is(0L));
+
+
+        assertThat(hands.get(2).getName(), is("Wriggly Snake"));
+        assertThat(hands.get(2).getPart1(), is(
+            Tile.S1.v + Tile.S2.v + Tile.S3.v + Tile.S4.v + Tile.S5.v + Tile.S6.v + Tile.S7.v + Tile.S8.v + Tile.S9.v +
+            Tile.NW.v + Tile.EW.v + Tile.SW.v + Tile.WW.v
+        ));
+        assertThat(hands.get(2).getPart2(), is(
+            Tile.S3.v
+        ));
+        assertThat(hands.get(2).getPart3(), is(0L));
+        assertThat(hands.get(2).getPart4(), is(0L));
+
     }
 
     @Test
@@ -63,6 +64,6 @@ public class HandLibraryTest {
 
         List<Hand> hands = HandLibrary.loadFromJson(jsonObject);
 
-        assertThat(hands.size(), is(1708));
+        assertThat(hands.size(), is(5476659));
     }
 }
